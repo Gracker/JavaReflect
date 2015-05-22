84f87883a679fcc157ddb0bc0d6c9b8baf1b7568
Jianwu Gao <553000664@qq.com>
2015-05-22 16:03:32 +0800

    display result to textview

--- app/src/main/java/com/gracker/javareflect/MainActivity.java
+++ app/src/main/java/com/gracker/javareflect/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -17,12 +18,18 @@ import java.lang.reflect.Modifier;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    public static final String ClASS_NAME = "com.gracker.javareflect.Person";
+    public static final String ClASS_NAME_SUPERMAN = "com.gracker.javareflect.SuperMan";
+
+    private TextView mTextView;
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.txt_reflect);
 
         try {
             demo0();
@@ -47,36 +54,50 @@ public class MainActivity extends AppCompatActivity {
      * 正常加载Person类
      */
     private void demo0() {
-        System.out.println("====================Demo0======================");
         TimeUtils.start();
+
         Person mPerson = new Person();
         mPerson.setAge("28");
         mPerson.setName("Xiao Ming");
-        System.out.println(mPerson.toString() + " Time = " + TimeUtils.end());
-        System.out.println("===============================================");
+
+        String result = "Demo0 : \n " + " " + mPerson.toString() + " \n  Time = " + TimeUtils.end();
+        mTextView.setText(result);
     }
 
     /**
      * 演示反射的两种写法
+     *
      * @throws ClassNotFoundException
      */
     private void demo1() throws ClassNotFoundException {
         //定义两个类型都未知的Class , 设置初值为null, 看看如何给它们赋值成Person类
+
+        TimeUtils.start();
         Class<?> class1;
         Class<?> class2;
 
         //写法1, 可能抛出 ClassNotFoundException [多用这个写法]
-        class1 = Class.forName("com.gracker.javareflect.Person");
-        System.out.println("====================Demo1======================");
+        class1 = Class.forName(ClASS_NAME);
 
-        System.out.println("Demo1:(写法1) 包名: " + class1.getPackage().getName() + "，"
-                + "完整类名: " + class1.getName());
+        String result1 = "\n*************\n"
+                + "Demo1.1 : get package name and class name  only\n "
+                + " PackageName = " + class1.getPackage().getName() + "\n"
+                + " ClassName = " + class1.getName() + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result1);
+
 
         //写法2
+        TimeUtils.start();
+
         class2 = Person.class;
-        System.out.println("Demo1:(写法2) 包名: " + class2.getPackage().getName() + "，"
-                + "完整类名: " + class2.getName());
-        System.out.println("===============================================");
+
+        String result2 = "\n*************\n"
+                + "Demo1.2 : get package name and class name  only\n "
+                + " PackageName = " + class2.getPackage().getName() + "\n"
+                + " ClassName = " + class2.getName() + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result2);
     }
 
     /**
@@ -86,21 +107,21 @@ public class MainActivity extends AppCompatActivity {
      * @throws IllegalAccessException
      * @throws InstantiationException
      */
-    public static void demo2() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public void demo2() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         TimeUtils.start();
         Class<?> class1;
-        class1 = Class.forName("com.gracker.javareflect.Person");
+        class1 = Class.forName(ClASS_NAME);
         //由于这里不能带参数，所以你要实例化的这个类Person，一定要有无参构造函数，后续的Demo会演示如何调用带参数的构造函数
         Person person = (Person) class1.newInstance();
         person.setAge("20");
-        person.setName("Gao Jianwu");
+        person.setName("Xiao Ming");
 
-        System.out.println("====================Demo2======================");
-        System.out.println("Demo2 构造没有参数的对象: " + person.getName() + " : " + person.getAge());
-        System.out.println("Time = " + TimeUtils.end());
-        System.out.println("===============================================");
+        String result = "\n ************* \n"
+                + "Demo2 : new instance with relfect " + "\n"
+                + " Person : " + person.toString() + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result);
     }
-
 
     /**
      * Demo3: 通过Java反射机制得到一个类的构造函数，并实现创建带参实例对象
@@ -111,12 +132,12 @@ public class MainActivity extends AppCompatActivity {
      * @throws InstantiationException
      * @throws IllegalArgumentException
      */
-    public static void demo3() throws ClassNotFoundException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    public void demo3() throws ClassNotFoundException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        TimeUtils.start();
         Class<?> class1;
         Person person1;
-        Person person2;
 
-        class1 = Class.forName("com.gracker.javareflect.Person");
+        class1 = Class.forName(ClASS_NAME);
         //得到一系列构造函数集合
         Constructor<?>[] constructors = class1.getConstructors();
 
@@ -124,14 +145,23 @@ public class MainActivity extends AppCompatActivity {
         person1.setAge("22");
         person1.setName("Gao Jianwu");
 
+        String result1 = "\n ************* \n"
+                + "Demo3.1 : new instance with constructor " + "\n"
+                + " Person : " + person1.toString() + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result1);
 
+
+        TimeUtils.start();
+        Person person2;
         person2 = (Person) constructors[1].newInstance("20", "Sun Yibo");
 
-        System.out.println("=====================Demo3=====================");
-        System.out.println("demo3 通过反射构造带参数的对象: " + person1.getName() + " : " + person1.getAge()
-                        + "  ,   " + person2.getName() + " : " + person2.getAge()
-        );
-        System.out.println("===============================================");
+
+        String result2 = "\n ************* \n"
+                + "Demo3.2 : new instance with constructor " + "\n"
+                + " Person : " + person2.toString() + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result2);
     }
 
     /**
@@ -144,25 +174,27 @@ public class MainActivity extends AppCompatActivity {
      * @throws InstantiationException
      * @throws ClassNotFoundException
      */
-    public static void demo4() throws IllegalArgumentException, IllegalAccessException, SecurityException, NoSuchFieldException, InstantiationException, ClassNotFoundException {
+    public void demo4() throws IllegalArgumentException, IllegalAccessException, SecurityException, NoSuchFieldException, InstantiationException, ClassNotFoundException {
+
+        TimeUtils.start();
         Class<?> class1;
-        class1 = Class.forName("com.gracker.javareflect.Person");
+        class1 = Class.forName(ClASS_NAME);
         Object obj = class1.newInstance();
 
         Field personNameField = class1.getDeclaredField("name");
         personNameField.setAccessible(true);
-        personNameField.set(obj, "Cui Yingyun");
+        personNameField.set(obj, "Xiao Hong");
 
         Field personAgeField = class1.getDeclaredField("age");
         personAgeField.setAccessible(true);
         personAgeField.set(obj, "30");
 
-        System.out.println("======================Demo4=====================");
-        System.out.println("demo4: 修改属性之后得到属性变量的值：Name = " + personNameField.get(obj));
-        System.out.println("demo4: 修改属性之后得到属性变量的值：Age = " + personAgeField.get(obj));
-        System.out.println("demo4: 修改属性之后得到属性变量的值：Person = " + obj.toString());
-        System.out.println("===============================================");
-
+        String result = "\n ************* \n"
+                + "Demo4 : field get and set " + "\n"
+                + " Person : " + "Name = " + personNameField.get(obj)
+                + " Age = " + personAgeField.get(obj) + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result);
     }
 
     /**
@@ -172,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public static void demo5() throws ClassNotFoundException {
         Class<?> class1;
-        class1 = Class.forName("com.gracker.javareflect.SuperMan");
+        class1 = Class.forName(ClASS_NAME);
 
         //取得父类名称
         Class<?> superClass = class1.getSuperclass();
@@ -219,19 +251,31 @@ public class MainActivity extends AppCompatActivity {
      * @throws IllegalArgumentException
      * @throws InstantiationException
      */
-    public static void demo6() throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public void demo6() throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        TimeUtils.start();
         Class<?> class1;
-        class1 = Class.forName("com.gracker.javareflect.SuperMan");
-        System.out.println("=====================Demo6=====================");
+        class1 = Class.forName(ClASS_NAME_SUPERMAN);
 
-        System.out.println("Demo7: \n调用无参方法fly()：");
         Method method = class1.getMethod("fly");
         method.invoke(class1.newInstance());
 
-        System.out.println("调用有参方法walk(int m)：");
-        method = class1.getMethod("fly", int.class);
-        method.invoke(class1.newInstance(), 100);
-        System.out.println("===============================================");
+        String result1 = "\n ************* \n"
+                + "Demo6.1 : invoke method " + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result1);
+
+
+        TimeUtils.start();
+        Class<?> class2;
+        class2 = Class.forName(ClASS_NAME_SUPERMAN);
+
+        method = class2.getMethod("fly", int.class);
+        method.invoke(class2.newInstance(), 100);
+
+        String result2 = "\n ************* \n"
+                + "Demo6.2 : invoke method " + "\n"
+                + " Time = " + TimeUtils.end();
+        mTextView.append(result2);
 
     }
 
